a6d939a86d56c634033ca59ecd079762d477985c
isaNgMiN <sleeplessinwhat@gmail.com>
2023-05-11 01:39:42 +0900

    init blog

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/sleeplessinwhat/Desktop/밈민/tech-blog/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/sleeplessinwhat/Desktop/밈민/tech-blog/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/sleeplessinwhat/Desktop/밈민/tech-blog/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
+  require("/Users/sleeplessinwhat/Desktop/밈민/tech-blog/src/css/custom.css"),
+];
